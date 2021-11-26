b6e9f06efbc57e5d4e4b940eccf60700525e7735
Gabuu43 <65136068+Gabuu43@users.noreply.github.com>
2021-11-26 20:11:55 -0200

    Update busca.php

--- includes/busca.php
+++ includes/busca.php
@@ -1,6 +1,5 @@
 <form class="form-inline my-2 my-lg-0" method='get' action=''>
     <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="busca" name='busca'>
     <button class="btn my-2 my-sm-0 text-dark" type="submit" style="background-color: yellow;"><b>Buscar<b></button>
-    <br>
-    
+
 </form>
