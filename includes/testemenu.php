dbbc8781eaca7231e0178866694cb8bf0ae70be1
Gabuu43 <65136068+Gabuu43@users.noreply.github.com>
2021-11-26 20:46:48 -0200

    Update testemenu.php

--- includes/testemenu.php
+++ includes/testemenu.php
@@ -2,12 +2,18 @@
   session_start();
 ?>
 <head>
+  <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet"> 
+  <link rel="stylesheet" type="text/css" href="estilos/estilos.css">
 </head>
 <nav class="navbar navbar-expand-lg navbar-light" style="min-height: 70px; background-color: black;">
+
   <a class="nav-link" href="index.php"><img src="img/LOGO.png" style="max-height: 40px;"></a>
+   <button class="bg-warning navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
