e9e54bbd5509363ddb5d5fb5a6534b0983d60558
Gabuu43 <65136068+Gabuu43@users.noreply.github.com>
2021-11-29 17:32:10 -0200

    Update testemenu.php

--- includes/testemenu.php
+++ includes/testemenu.php
@@ -14,7 +14,7 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="min-height: 70px; background-color: black;">
 
   <a class="nav-link" href="index.php"><img src="img/LOGO.png" style="max-height: 40px;"></a>
-   <button class="bg-warning navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <button class="navbar-toggler" style="background-color: yellow;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
